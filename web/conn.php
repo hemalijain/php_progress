84d19b828dd8fea10752122ea27e2861cec2d875
hemalijain <hemalijains@gmail.com>
2017-08-15 19:22:21 +0530

    read from env variables

--- web/conn.php
+++ web/conn.php
@@ -1,9 +1,8 @@
 <?php
-DEFINE('DB_USERNAME', 'root');
- DEFINE('DB_PASSWORD', 'root');
- DEFINE('DB_HOST', 'localhost');
- DEFINE('DB_DATABASE', 'phptest');
-
+  DEFINE('DB_USERNAME', $_ENV["DB_USER"]);
+ DEFINE('DB_PASSWORD', $_ENV["DB_PASSWORD"]);
+ DEFINE('DB_HOST', $_ENV["DB_HOST"]);
+ DEFINE('DB_DATABASE', $_ENV["DB_NAME"]);
  $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
  if (mysqli_connect_error()) {
